e7a2b66d8d37cb3ff726bba8d995e2f6eb53b393
Joselucas18 <lucaslukinha662@gmail.com>
2021-11-14 19:28:50 +0000

    connection class

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 require_once './vendor/autoload.php';
 
-use PDO;
+use ExemploPDOMySQL\MySQLConnection;
 
-$bd = new PDO('mysql:host=localhost;dbname=biblioteca', 'root', '');
+$bd = new MySQLConnection();
 
 $comando = $bd->prepare('SELECT * FROM generos');
 $comando-> execute();
